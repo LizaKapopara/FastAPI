bd638feafd317249284da1bcdf1a41317ace75e0
LizaKapopara <lizakapopara8@gmail.com>
2025-01-07 19:02:59 +0530

    add post method
    add post method

--- fastapi/main.py
+++ fastapi/main.py
@@ -7,6 +7,10 @@ def root():
     return {"message":"Helloo, Liza Kapopara!"}
 
 
-@app.get("/post")
+@app.get("/")
 def get_posts():
     return {"data":"This is your post"}
+
+@app.post("/createposts")
+def create_posts():
+    return{"message":"successfully created post"}
