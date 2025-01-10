a44e77fb7c9806b2f46a80cefbb612c26542e44b
LizaKapopara <lizakapopara8@gmail.com>
2025-01-10 12:15:56 +0530

    learning path and order matters
    learning path and order matters

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,4 +1,14 @@
+from typing import Optional
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+from random import randrange
+
+class liza(BaseModel):
+    name : str
+    age : int
+    published: bool = True
+    rating: Optional[int] = None
 
 app = FastAPI()
 
@@ -6,11 +16,44 @@ app = FastAPI()
 def root():
     return {"message":"Helloo, Liza Kapopara!"}
 
+my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1},{"title": "favourite foods", "content": "I like pizza", "id": 2}]
 
-@app.get("/")
+def find_post(id):
+    for p in my_posts:
+        if p["id"] == id:
+            return p
+
+@app.get("/posts")
 def get_posts():
-    return {"data":"This is your post"}
+    return {"data":my_posts}
 
 @app.post("/createposts")
-def create_posts():
-    return{"message":"successfully created post"}
+def create_posts(payload: dict = Body(...)):
+    print(payload)
+    return{"new_post":f"title {payload['title']} content: {payload['content']}"}
+
+
+
+@app.post("/gettingposts")
+def getting_posts(new_post: liza):
+    print(new_post)
+    print(new_post.dict())
+    return {"data": new_post}
+
+@app.post("/posts2")
+def create_postss(post: liza):
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0, 1000000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
+
+@app.get("/posts/latest")
+def get_latest_post():
+    post = my_posts[len(my_posts)-1]
+    return {"detail": post}
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    post = find_post(id)
+    print(post)
+    return {"post_detail": post}
