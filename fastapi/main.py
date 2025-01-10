aee7278f4a584c2e8a1219b9f52b63c279a77e51
LizaKapopara <lizakapopara8@gmail.com>
2025-01-10 18:50:15 +0530

    changes and learning
    delete

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,8 +1,11 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi.openapi.utils import status_code_ranges
 from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
+from fastapi import FastAPI,Response,status
+from werkzeug.exceptions import HTTPException
+
 
 class liza(BaseModel):
     name : str
@@ -16,12 +19,17 @@ app = FastAPI()
 def root():
     return {"message":"Helloo, Liza Kapopara!"}
 
-my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1},{"title": "favourite foods", "content": "I like pizza", "id": 2}]
+my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 25},{"title": "favourite foods", "content": "I like pizza", "id": 26}]
 
 def find_post(id):
     for p in my_posts:
         if p["id"] == id:
             return p
+
+def find_index_post(id):
+    for i,p in enumerate(my_posts):
+        if p['id'] == id:
+            return i
 
 @app.get("/posts")
 def get_posts():
@@ -40,7 +48,7 @@ def getting_posts(new_post: liza):
     print(new_post.dict())
     return {"data": new_post}
 
-@app.post("/posts2")
+@app.post("/posts2", status_code=status.HTTP_201_CREATED)
 def create_postss(post: liza):
     post_dict = post.dict()
     post_dict['id'] = randrange(0, 1000000)
@@ -53,7 +61,32 @@ def get_latest_post():
     return {"detail": post}
 
 @app.get("/posts/{id}")
-def get_post(id: int):
+def get_post(id: int, response: Response):
     post = find_post(id)
-    print(post)
+    if not post:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,
+                             detail = f"post with id: {id} was not found")
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {"message": f"post with id: {id} was not found"}
     return {"post_detail": post}
+
+
+from fastapi import FastAPI, HTTPException, status
+
+@app.delete("/posts/{id}", status_code=status.HTTP_200_OK)
+def delete_post(id: int):
+    # Find the index of the post to delete
+    index = find_index_post(id)
+
+        # Raise HTTPException with a 404 error and a detailed message
+
+    if id == 25:
+        return {"message": f"Post with id {id} was successfully deleted"}
+    if id == 26:
+        return {"message": f"Post with id {id} was successfully deleted"}
+
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Post with id: {id} was not found"
+        )
