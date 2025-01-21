47d70ad992937210f39e0e6bb4ec78d1b66faf7c
LizaKapopara <lizakapopara8@gmail.com>
2025-01-21 19:00:20 +0530

    changes
    changes

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -1,24 +1,25 @@
+from dataclasses import Field
 from typing import Optional
 from fastapi.openapi.utils import status_code_ranges
 from fastapi.params import Body
-from pydantic import BaseModel
+from google.protobuf.descriptor import Descriptor
+from pydantic import BaseModel,Field
 from random import randrange
-from fastapi import FastAPI,Response,status,HTTPException
+
+from streamlit import title
+
+from fastapi import FastAPI, Response, status, HTTPException
 # from werkzeug.exceptions import HTTPException
 import psycopg2
 from psycopg2.extras import RealDictCursor
-
-class liza(BaseModel):
-    title : str
-    content : str
-    published: bool = True
+import time
 
 app = FastAPI(docs_url="/liza")
 
 class liza(BaseModel):
-    title : str
-    content : str
-    published: bool = True
+    title : Optional[str] = Field(None)
+    content : Optional[str]  = Field(None,description= "asdfkasdfasdfasdfasd")
+    published : Optional[bool] = Field(None)
 
 try:
     conn = psycopg2.connect(host= 'localhost', database='fastapi', user='postgres', password='postgres', cursor_factory=RealDictCursor)
@@ -28,10 +29,8 @@ try:
 except Exception as error:
     print("Connecting the database failed")
     print("Error:", error)
+    time.sleep(2)
 
-@app.get("/")
-def root():
-    return {"message":"Helloo, Liza Kapopara!"}
 
 my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 25},{"title": "favourite foods", "content": "I like pizza", "id": 26}]
 
@@ -52,7 +51,10 @@ def find_index_post(id):
 
 @app.get("/posts")
 def get_posts():
-    return {"data":my_posts}
+    cursor.execute("""SELECT * FROM posts """)
+    posts = cursor.fetchall()
+    print(posts)
+    return {"data":posts}
 
 @app.post("/createposts")
 def create_posts(payload: dict = Body(...)):
@@ -64,6 +66,14 @@ def getting_posts(new_post: liza):
     print(new_post)
     print(new_post.dict())
     return {"data": new_post}
+
+@app.post("/posts3", status_code=status.HTTP_201_CREATED)
+def create_posts(post: liza):
+    cursor.execute(""" insert into posts (title, content, published) values (%s, %s, %s) returning *""", (post.title, post.content, post.published))
+    new_post = cursor.fetchone()
+    conn.commit()
+    return {"data": new_post}
+
 
 @app.post("/posts2", status_code=status.HTTP_201_CREATED)
 def create_postss(post: liza):
@@ -78,8 +88,9 @@ def get_latest_post():
     return {"detail": post}
 
 @app.get("/posts/{id}")
-def get_post(id: int, response: Response):
-    post = find_post(id)
+def get_post(id: str):
+    cursor.execute(""" select * from posts where id = %s""", (str(id)))
+    post = cursor.fetchone()
     if not post:
         raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,
                              detail = f"post with id: {id} was not found")
@@ -89,7 +100,7 @@ def get_post(id: int, response: Response):
 
 @app.delete("/posts/{id}", status_code=status.HTTP_200_OK)
 def delete_post(id: int):
-    # Find the index of the post to delete
+
     index = find_index_post(id)
 
         # Raise HTTPException with a 404 error and a detailed message
@@ -107,12 +118,14 @@ def delete_post(id: int):
 
 @app.delete("/xyz/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_posts(id: int):
-    index = find_index_post(id)
 
-    if index == None:
+    cursor.execute(""" delete from posts where id = %s returning * """, (str(id)))
+    deleted_post = cursor.fetchone()
+    conn.commit()
+
+    if deleted_post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} is not exists")
 
-    my_posts.pop(index)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @app.put(f"/posts/{id}")
@@ -129,4 +142,23 @@ def update_post(id: int,post: liza):
 
     # print(post)
     # return {'message': "updated post"}
+
+@app.put("/xyz/post/{id}")
+def update_post(id: int, post: liza):
+    content = post.content
+    title = post.title
+    print(title,content)
+
+    cursor.execute(
+        f"""UPDATE posts SET
+            content = CASE WHEN %s IS NOT NULL THEN %s ELSE content END,
+            title = CASE WHEN %s IS NOT NULL THEN %s ELSE title END,
+            published = CASE WHEN %s IS NOT NULL THEN %s ELSE published END where id = %s""",(post.content, post.content, post.title, post.title, post.published, post.published, id))
+    conn.commit()
+
+    # if not update_post:
+    #     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} is not exists")
+
+    return {"updated_data": "sfg"}
+
 
